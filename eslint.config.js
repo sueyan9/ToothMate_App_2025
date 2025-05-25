586b4c8383cb451bfa1db4a210877d14e3dde16e
sueyan9 <409133742@qq.com>
2025-05-26 11:24:02 +1200

    test

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,23 @@
-// https://docs.expo.dev/guides/using-eslint/
 const { defineConfig } = require('eslint/config');
 const expoConfig = require('eslint-config-expo/flat');
 
 module.exports = defineConfig([
   expoConfig,
   {
-    ignores: ['dist/*'],
+    ignores: ['dist/*', 'build/*'],
   },
+  {
+    files: [
+      '**/*.test.js',
+      '**/*.spec.js',
+      'test/**/*.js',
+      'tests/**/*.js'
+    ],
+    env: {
+      jest: ture,
+    },
+    rules:{
+
+    }
+  }
 ]);
