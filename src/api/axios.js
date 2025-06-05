ec28a5caccd9873830b5ac7241f597faa4ff749d
XA1626 <132309726+XA1626@users.noreply.github.com>
2025-06-05 13:34:18 +1200

    v0.0.8

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'https://3b09-156-62-117-17.ngrok-free.app  ',
+  baseURL: 'https://11dd-27-252-150-214.ngrok-free.app ',
 });
