bb939f2accce96dc1f7fbf390828bee21198a54e
sueyan9 <409133742@qq.com>
2025-06-29 18:54:31 +1200

    WIP: 修改 axios.js 和 DentalChartScreen.jsx

--- src/screens/DentalChartScreen/DentalChartScreen.jsx
+++ src/screens/DentalChartScreen/DentalChartScreen.jsx
@@ -5,7 +5,7 @@ const DentalChartScreen = () => (
     <View style={{ flex: 1 }}>
       {/* <Text style={styles.header}>3D Mouth Model</Text> */}
       <WebView
-          source={{ uri: 'https://304c-156-62-117-28.ngrok-free.app' }} // H5 page url
+          source={{ uri: 'http://192.168.1.166:3001/' }} // H5 page url
           style={{ flex: 1 }}
       />
     </View>
