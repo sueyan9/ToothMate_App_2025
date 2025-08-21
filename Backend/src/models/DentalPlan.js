34ed11ddf1770a7c043274b7e55a754f48214b83
sueyan9 <409133742@qq.com>
2025-08-21 14:50:55 +1200

    API for backend

--- Backend/src/models/DentalPlan.js
+++ Backend/src/models/DentalPlan.js
@@ -2,9 +2,16 @@ const mongoose = require("mongoose");
 
 const dentalPlanSchema = new mongoose.Schema({
     userNhi: { type: String, required: true },
-    createdAt: { type: Date, default: Date.now },
-    title: { type: String }, // e.g., "Initial Treatment Plan"
-    status: { type: String, enum: ['active', 'completed'], default: 'active' },
+    planName: { type: String, required: true },
+    startDate: { type: Date, default: Date.now },
+    endDate: { type: Date },
+    planType: { type: String }, // e.g. "root-canal", "filling", "cleaning"
+    steps: [
+        {
+            name: { type: String, required: true }, // step name
+            description: { type: String },
+            status: { type: String, enum: ["planned", "in-progress", "completed"], default: "planned" },
+        },
+    ],
 });
-
 module.exports = mongoose.model("DentalPlan", dentalPlanSchema);
