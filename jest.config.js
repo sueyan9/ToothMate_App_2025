a98b8629884e660e795b3994b33a89ad0c7a7065
William Sukjaem <133365639+Willsu00@users.noreply.github.com>
2025-09-11 16:57:31 +1200

    jest test for UserScreen.jsx

--- jest.config.js
+++ jest.config.js
@@ -5,7 +5,7 @@ module.exports = {
         '^.+\\.[jt]sx?$': 'babel-jest',
     },
     transformIgnorePatterns: [
-        'node_modules/(?!(expo-modules-core|@expo-google-fonts|@expo|expo|expo-font|expo-constants|expo-status-bar|expo-asset|expo-linear-gradient|react-native|@react-native|react-navigation)/)',
+        'node_modules/(?!(expo-modules-core|@expo-google-fonts|@expo|expo|expo-font|expo-constants|expo-status-bar|expo-asset|expo-linear-gradient|react-native|@react-native|react-navigation|react-native-elements|react-native-keyboard-aware-scroll-view|react-native-modal-datetime-picker|react-native-size-matters|dayjs)/)',
     ],
     moduleNameMapper: {
         '\\.css$': 'identity-obj-proxy',
