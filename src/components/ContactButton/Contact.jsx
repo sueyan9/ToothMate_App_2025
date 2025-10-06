a85acb3afe8bb1c1fe32ff7abe74fe10f6bbfc57
Abby Sutton <abbysutton09@gmail.com>
2025-10-07 09:29:15 +1300

    button change
    phone and email buttons that link directly to the phone

--- src/components/ContactButton/Contact.jsx
+++ src/components/ContactButton/Contact.jsx
@@ -1,7 +1,7 @@
-import { Ionicons, MaterialCommunityIcons } from '@expo/vector-icons';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 import { useFocusEffect } from '@react-navigation/native';
-import React, { useContext, useState } from 'react';
-import { Linking, Modal, Text, TouchableOpacity, View } from 'react-native';
+import React, { useContext } from 'react';
+import { Linking, TouchableOpacity } from 'react-native';
 import { Context as UserContext } from '../../context/UserContext/UserContext';
 import styles from './styles';
 
@@ -26,36 +26,15 @@ const Contact = () => {
             }, [])
         );
 
-    const [modalShown, setModalShown] = useState(false);
-
     return (
         <>
-        <TouchableOpacity style={styles.button} onPress={() => setModalShown(true)}>
-            <MaterialCommunityIcons name='phone' size={24} color={'#E9F1F8'} style={styles.phone}/>
-        </TouchableOpacity>
+            <TouchableOpacity style={styles.buttonPhone} onPress={() => Linking.openURL(`tel:${clinic?.phone}`)}>
+                <MaterialCommunityIcons name='phone' size={24} color={'#E9F1F8'} style={styles.phone}/>
+            </TouchableOpacity>
 
-        <Modal animationType='slide' transparent={true} visible={modalShown} onRequestClose={() => setModalShown(false)}>
-            <View style={styles.overlay}>
-                <View style={styles.modalContainer}>
-                    <View style={styles.modalHeader}>
-                        <Text style={styles.modalTitle}>{clinic?.name} Contact Details</Text>
-                        <TouchableOpacity 
-                            style={styles.closeButton}
-                            onPress={() => setModalShown(false)}>
-                            <Ionicons name="close" size={24} color="#333333" />
-                        </TouchableOpacity>
-                    </View>
-                    <View style={styles.details}>
-                        <Text style={styles.detailType}>Phone:</Text>
-                        <Text style={styles.detailInfo} onPress={() => Linking.openURL(`tel:${clinic?.phone}`)}>{clinic?.phone}</Text>
-                    </View>
-                    <View style={styles.details}>
-                        <Text style={styles.detailType}>Email:</Text>
-                        <Text style={styles.detailInfo} onPress={() => Linking.openURL(`mailto:${clinic?.email}`)}>{clinic?.email}</Text>
-                    </View>
-                </View>
-            </View>
-        </Modal>
+            <TouchableOpacity style={styles.buttonMail} onPress={() => Linking.openURL(`mailto:${clinic?.email}`)}>
+                <MaterialCommunityIcons name='mail' size={24} color={'#E9F1F8'} style={styles.phone}/>
+            </TouchableOpacity>
         </>
     );
 };
--- src/components/ContactButton/styles.js
+++ src/components/ContactButton/styles.js
@@ -1,9 +1,17 @@
 import { StyleSheet } from 'react-native';
 
 export default StyleSheet.create({
-    button: {
-        width: 40,
-        height: 40,
+    buttonPhone: {
+        width: 35,
+        height: 35,
+        borderRadius: 50,
+        marginRight: 8,
+        backgroundColor: '#875B51',
+        justifyContent: 'center'
+    },
+    buttonMail: {
+        width: 35,
+        height: 35,
         borderRadius: 50,
         marginRight: 24,
         backgroundColor: '#875B51',
@@ -13,59 +21,4 @@ export default StyleSheet.create({
         alignSelf: 'center',
         justifyContent: 'center'
     },
-    modalContainer: {
-        backgroundColor: '#FFFDF6',
-        borderRadius: 16,
-        padding: 20,
-        width: '90%',
-        maxHeight: '80%',
-    },
-    overlay: {
-        flex: 1,
-        backgroundColor: 'rgba(0, 0, 0, 0.5)',
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    modalHeader: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        marginBottom: 20,
-        paddingBottom: 12,
-        borderBottomWidth: 1,
-        borderBottomColor: '#EDDFD3',
-    },
-    modalTitle: {
-        fontSize: 20,
-        fontWeight: 'bold',
-        color: '#333333',
-        flexWrap: 'wrap',
-        maxWidth: '90%',
-        flexShrink: 1,
-        textAlign: 'center'
-    },
-    details: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        paddingLeft: 8,
-        paddingRight: 8,
-        flexWrap: 'wrap',
-        rowGap: 20,
-    },
-    detailInfo: {
-        fontSize: 16,
-        color: '#875B51',
-        textAlign: 'right',
-        maxWidth: '80%',
-        flex: 2,
-        flexShrink: 1,
-        textDecorationLine: 'underline'
-    },
-    detailType: {
-        fontSize: 16,
-        color: '#333333',
-        textAlign: 'left',
-        flex: 1,
-        flexShrink: 0
-    }
 });
