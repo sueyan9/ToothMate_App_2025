1cbeb564995d563e3586f37a4149aaaf4a91b8c3
Abby Sutton <abbysutton09@gmail.com>
2025-10-16 00:32:37 +1300

    button fix

--- react-dental-chart/src/components/ToothInformation.js
+++ react-dental-chart/src/components/ToothInformation.js
@@ -272,7 +272,7 @@ export default function ToothInformation({ toothInfo }) {
           style={{
             position: 'fixed',
             right: '24px',
-            bottom: isOpen ? 'calc(100% - 380px)' : '175px',
+            bottom: isOpen ? 'calc(100% - 350px)' : '175px',
             padding: '8px',
             height: '82px',
             width: '82px',
